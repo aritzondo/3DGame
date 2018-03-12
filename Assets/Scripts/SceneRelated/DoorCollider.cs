3aab5306be8308f3c14919b5762c6c97a9eaeba7
Álvaro Tomás <auberontermeraz@gmail.com>
2018-03-12 18:07:45 +0100

    Apañao el Hub con todo ordenadito y los colliders de las puertas puestos donde toca, pero no van

---
+++ Assets/Scripts/SceneRelated/DoorCollider.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorCollider : MonoBehaviour {
+
+    public GameObject door;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("door"))
+        {
+            door = other.gameObject;
+            Debug.Log("Door is opening");
+            //door.GetComponent<DoorMovement>();
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("door"))
+        {
+            door = other.gameObject;
+            Debug.Log("Door is closing");
+            //door.GetComponent<DoorMovement>();
+        }
+    }
+}
