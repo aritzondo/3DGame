55fc27d9e0c3425cd80de96ee5dd8bab8437d91c
Javier Veron <verontavasco@gmail.com>
2018-04-08 21:32:51 +0200

    some shit in the levelV

---
+++ Assets/RotateBridge.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateBridge : MonoBehaviour {
+
+    public GameObject blueFloor;
+
+    private bool blueUp = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (blueUp)
+        {
+            blueFloor.SetActive(false);
+        }
+        else
+        {
+            blueFloor.SetActive(true);
+        }
+	}
+}
