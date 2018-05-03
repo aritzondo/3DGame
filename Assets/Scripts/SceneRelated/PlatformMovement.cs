191de98511ebcb3be75d0200123434ff07babcbb
Álvaro Tomás <auberontermeraz@gmail.com>
2018-05-03 18:32:57 +0200

    Plataformas
    Escena para testear el movimiento de las plataformas.
    Añadido a medias, le falta decelerar cuando llegue a un waypoint, pero funcionar funciona.

--- Assets/Scripts/SceneRelated/PlatformMovement.cs
+++ Assets/Scripts/SceneRelated/PlatformMovement.cs
@@ -4,44 +4,68 @@ using UnityEngine;
 
 public class PlatformMovement : MonoBehaviour {
     #region PUBLIC VARIABLES
-    public float speed;
-    public Vector3 initialPosition;
-    public float maxDistance = 20.0f;
+    public Transform[] waypoints;
+    public float maxDist;
+    public float maxSpeed;
+    public float accel;
     #endregion
 
     #region PRIVATE VARIABLES
+    private bool canMove;
+    private int currentWaypoint;
+    private float velocity;
+    private float dist;
     private float orientation;
-    
     #endregion
 
     #region PROPERTIES
-
+    public bool CanMove
+    {
+        get { return canMove; }
+        set { canMove = value; }
+    }
     #endregion
 
     // Use this for initialization
-    void Start () {
-        orientation = 1.0f;
-        initialPosition = transform.position;
-
+    void Start ()
+    {
+        transform.position = waypoints[0].position;
+        currentWaypoint = 0;
+        canMove = true;
     }
 
     // Update is called once per frame
-    void Update () {
-        if(Mathf.Approximately(initialPosition.z + maxDistance, transform.position.z))
+    void Update ()
+    {
+        Debug.Log(orientation);
+        if (canMove)
         {
-            orientation = -1.0f;
+            MovePlatform();
         }
-
-        if(transform.position == initialPosition)
-        {
-            orientation = 1.0f;
-        }
-
-        transform.position += transform.forward * orientation * speed;
 	}
 	
 	#region METHODS
 	
+    public void MovePlatform()
+    {
+        float dt = Time.deltaTime;
+
+        Vector3 direction = waypoints[currentWaypoint].position - transform.position;
+        dist = direction.magnitude;
+                
+        if (dist <= maxDist)
+        {
+            currentWaypoint = (currentWaypoint + 1) % waypoints.Length;
+            orientation = 0;
+
+        }
+
+        orientation = Mathf.Clamp(dist, -1, 1);
+        float velOffset = (maxSpeed * orientation) - velocity;
+        velocity += Mathf.Clamp(velOffset, -accel * dt, accel * dt);
+        transform.position = Vector3.MoveTowards(transform.position, waypoints[currentWaypoint].position, velocity * dt);
+    }
+
 	#endregion
 	
 }
