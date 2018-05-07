c0da84015315f23663f2fd4361480e5efeef91d1
Aritz Valencia Quiroga <aritzondovq@msn.com>
2018-05-07 17:58:48 +0200

    empezado los escrpits para mover objetos mientras clickas con el raton

--- Assets/Scripts/Character/PlayerClicker.cs
+++ Assets/Scripts/Character/PlayerClicker.cs
@@ -4,22 +4,38 @@ using UnityEngine;
 
 public class PlayerClicker : MonoBehaviour {
 
+    public Vector3 carryOffset = new Vector3(0,1,1);
     public Camera cam;
+
+    private bool carrying = false;
     
 	// When the mouse is pressed we throw a ray to the point were it has been clicked
     // and if its a rotator we activate it
 	void Update () {
         if (Input.GetMouseButtonDown(0))
         {
-            //Cast a ray from the camera to his forward
-            Ray ray = new Ray(cam.transform.position, cam.transform.forward);
-            RaycastHit hit;
-            if(Physics.Raycast(ray, out hit, 200.0f))
+            if (carrying)
             {
-                //if the ray hits a rotable object we call to his method to activate it
-                if (hit.transform.gameObject.tag == "rotable")
+                GetComponentInChildren<Movable>().Release();
+            }
+            else
+            {
+                //Cast a ray from the camera to his forward
+                Ray ray = new Ray(cam.transform.position, cam.transform.forward);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit, 10.0f))
                 {
-                    hit.transform.gameObject.GetComponent<MovingWithMouse>().clicked();
+                    MovingWithMouse movingScript = hit.transform.gameObject.GetComponent<MovingWithMouse>();
+                    if (movingScript != null)
+                    {
+                        movingScript.clicked();
+                    }
+
+                    Movable moveScript = hit.transform.gameObject.GetComponent<Movable>();
+                    if (moveScript != null)
+                    {
+                        moveScript.Carry(transform, carryOffset);
+                    }
                 }
             }
         }
---
+++ Assets/Scripts/Interaction/Movable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movable : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Carry(Transform parent, Vector3 offset)
+    {
+        transform.SetParent(parent);
+        transform.localPosition = offset;
+    }
+
+    public void Release()
+    {
+        transform.parent = null;
+    }
+}
