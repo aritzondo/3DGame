9fea9e12f51f62abd821efb5b973c09d5d6e5f3a
Jorge Chueca <jchueca_96@hotmail.com>
2018-05-18 09:32:30 +0200

    no message

---
+++ Assets/Scripts/Managers/LevelOrganizer.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+
+[ExecuteInEditMode]
+public class LevelOrganizer : MonoBehaviour
+{
+#if UNITY_EDITOR
+
+    private Scene sceneOrganizer;
+    private string[] scene_paths;
+
+    void Start()
+    {
+        sceneOrganizer = EditorSceneManager.GetSceneByName("Organizer");
+
+
+        if (!Application.isPlaying && EditorSceneManager.GetActiveScene() == sceneOrganizer)
+        {
+            int number_scenes = UnityEngine.SceneManagement.SceneManager.sceneCountInBuildSettings;
+            Debug.Log(number_scenes);
+
+            scene_paths = new string[number_scenes];
+
+            for (int i = 0; i < number_scenes; i++)
+            {
+                scene_paths[i] = UnityEngine.SceneManagement.SceneUtility.GetScenePathByBuildIndex(i);
+                string toLoadScene_path = scene_paths[i];
+                Debug.Log("LOAD Scene: " + toLoadScene_path);
+
+                //loadScene(toLoadScene_path);
+            }
+
+        }
+    }
+
+    private void loadScene(string path)
+    {
+        EditorSceneManager.OpenScene(path, OpenSceneMode.Additive);
+    }
+
+#endif
+}
+
