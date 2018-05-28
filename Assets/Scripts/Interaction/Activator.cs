7e850c89a283629e16ecdfa9ce5c49eee165e1e1
luis <lusor97@gmail.com>
2018-05-28 13:58:33 +0200

    nuevo nivel

---
+++ Assets/Scripts/Interaction/Activator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activator : Activable
+{
+
+    private Animator anim;
+
+	void Start ()
+    {
+        anim = GetComponent<Animator>();
+	}
+
+    public override void Activate()
+    {
+        anim.SetBool("Active",true);
+    }
+}
