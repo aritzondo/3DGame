ddcfd2b40d3fc3b630c110951a2a7b2ebe32b210
Aritz Valencia Quiroga <aritzondovq@msn.com>
2018-06-01 22:40:08 +0200

    añadido trigger que informa del nivel

---
+++ Assets/InformNextLevel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InformNextLevel : MonoBehaviour {
+
+    public DoorMovement mDoor;
+
+    private int nextLevel;
+
+	// Use this for initialization
+	void Start () {
+        nextLevel = mDoor.sceneIndex;
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            DoorFrameManager.GetInstance().NewLevel(nextLevel);
+            Debug.Log(nextLevel);
+        }
+    }
+}
