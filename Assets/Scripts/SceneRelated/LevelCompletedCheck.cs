5c28b492ea27c53ccb2e67810373166a7aec499d
Álvaro Tomás <auberontermeraz@gmail.com>
2018-06-01 23:26:20 +0200

    FadeOut se accede mediante el bool del PauseMenu poniendolo a true

---
+++ Assets/Scripts/SceneRelated/LevelCompletedCheck.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelCompletedCheck : MonoBehaviour {
+
+    public Material finishedColor;
+    public Material closedDoor;
+
+    public void ClosedLevel()
+    {
+        GetComponent<Renderer>().material = closedDoor;
+    }
+
+    public void FinishedLevel()
+    {
+        GetComponent<Renderer>().material = finishedColor;
+    }
+}
