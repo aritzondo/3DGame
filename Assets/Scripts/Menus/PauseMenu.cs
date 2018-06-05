fe4f97be4e286b7e12a0bdb55bf106d2fbe31806
luis <lusor97@gmail.com>
2018-06-05 23:16:38 +0200

    cambios en el pause menu para consola

--- Assets/Scripts/Menus/PauseMenu.cs
+++ Assets/Scripts/Menus/PauseMenu.cs
@@ -40,7 +40,7 @@ public class PauseMenu : MonoBehaviour
             }
         }
 
-		if(Input.GetKeyDown(KeyCode.Escape))
+		if(Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Joystick1Button7))
         {
             if (isPaused)
             {
