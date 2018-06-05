5f223faa662f15bf44f0d88cc9da335e7bd6cd56
Javier Veron <verontavasco@gmail.com>
2018-06-05 22:18:06 +0200

    no message

--- Assets/Scripts/Character/PlayerClicker.cs
+++ Assets/Scripts/Character/PlayerClicker.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 
 public class PlayerClicker : MonoBehaviour
 {
-    
-    public Vector3 carryOffset = new Vector3(0,1,1);
+    //PC
+    public Vector3 carryOffset = new Vector3(0, 1, 1);
     public Camera cam;
     public float maxDistance = 3.0f;
 
+    //PC
     private CharacterMovement moveScript;
-
     private bool carrying = false;
     private bool canDrop = false;
     private DropTrigger dropSite;
+    //Android
+    private float startTime;
+    private float endTime;
+    private float swipeTime;
+    private float minTimeToBeSwipe = 0.1f;
+    private Vector3 startPos;
+    private Vector3 endPos;
+    private float swipeDistance;
 
     public DropTrigger DropSite
     {
@@ -38,7 +46,13 @@ public class PlayerClicker : MonoBehaviour
 
     // When the mouse is pressed we throw a ray to the point were it has been clicked
     // and if its a rotator we activate it
-    void Update () {
+    void Update()
+    {
+        Android();
+    }
+
+    private void PC()
+    {
         if (!moveScript.interacting)
         {
             if (Input.GetMouseButtonDown(0))
@@ -93,95 +107,85 @@ public class PlayerClicker : MonoBehaviour
             }
         }
     }
-    /*
-    public Vector3 carryOffset = new Vector3(0, 1, 1);
-    public Camera cam;
-    public float maxDistance = 3.0f;
 
-    private CharacterMovementMobile moveScript;
-
-    private bool carrying = false;
-    private bool canDrop = false;
-    private DropTrigger dropSite;
-
-    public DropTrigger DropSite
+    private void Android()
     {
-        set { dropSite = value; }
-    }
-
-    public bool CanDrop
-    {
-        //get { return CanDrop; }
-        set { canDrop = value; }
-    }
-
-    public bool Carrying
-    {
-        get { return carrying; }
-    }
-
-    private void Start()
-    {
-        moveScript = gameObject.GetComponent<CharacterMovementMobile>();
-    }
-
-    // When the mouse is pressed we throw a ray to the point were it has been clicked
-    // and if its a rotator we activate it
-    void Update()
-    {
-        if (!moveScript.interacting)
+        if (Input.touchCount > 0)
         {
-            if (Input.GetMouseButtonDown(0) && Input.touches.Length == 1)
+            float dt = Time.deltaTime;
+
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
             {
-                if (carrying && canDrop)
+                startTime = Time.time;
+                startPos = touch.position;
+            }
+
+            else if (touch.phase == TouchPhase.Ended)
+            {
+                endTime = Time.time;
+                endPos = touch.position;
+
+                swipeDistance = (endPos - startPos).magnitude;
+                swipeTime = (endTime - startTime);
+
+                if (swipeTime <= minTimeToBeSwipe)
                 {
-                    Movable carriedObj = GetComponentInChildren<Movable>();
-                    carriedObj.hook = dropSite;
-                    carriedObj.Release();
-                    carrying = false;
-                }
-                else if (!carrying)
-                {
-                    //Cast a ray from the camera to his forward
-                    Ray ray = new Ray(cam.transform.position, cam.transform.forward);
-                    RaycastHit hit;
-                    if (Physics.Raycast(ray, out hit, maxDistance))
+                    if (carrying && canDrop)
                     {
-                        Movable moveScript = hit.transform.gameObject.GetComponent<Movable>();
-                        if (moveScript != null && moveScript.isActiveAndEnabled)
+                        Movable carriedObj = GetComponentInChildren<Movable>();
+                        carriedObj.hook = dropSite;
+                        carriedObj.Release();
+                        carrying = false;
+                    }
+                    else if (!carrying)
+                    {
+                        //Cast a ray from the camera to his forward
+                        Ray ray = new Ray(cam.transform.position, cam.transform.forward);
+                        RaycastHit hit;
+                        if (Physics.Raycast(ray, out hit, maxDistance))
                         {
-                            Rotable rot = hit.transform.GetComponent<Rotable>();
-                            if (rot != null && rot.isIdle())
+                            Movable moveScript = hit.transform.gameObject.GetComponent<Movable>();
+                            if (moveScript != null && moveScript.isActiveAndEnabled)
                             {
-                                moveScript.Carry(cam.transform, carryOffset);
-                                carrying = true;
+                                Rotable rot = hit.transform.GetComponent<Rotable>();
+                                if (rot != null && rot.isIdle())
+                                {
+                                    moveScript.Carry(cam.transform, carryOffset);
+                                    carrying = true;
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Debug.Log("Cant drop");
+                    }
+                }
+            }
+
+            else if (touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Stationary)
+            {
+                swipeTime = (Time.time - startTime);
+                if (swipeTime > minTimeToBeSwipe)
+                {
+                    if (!carrying)
+                    {
+                        //Cast a ray from the camera to his forward
+                        Ray ray = new Ray(cam.transform.position, cam.transform.forward);
+                        RaycastHit hit;
+                        if (Physics.Raycast(ray, out hit, maxDistance))
+                        {
+                            Rotable rotateScript = hit.transform.gameObject.GetComponent<Rotable>();
+                            if (rotateScript != null && rotateScript.isActiveAndEnabled)
+                            {
+                                rotateScript.clickedAndroid(touch);
                             }
                         }
                     }
                 }
-                else
-                {
-                    Debug.Log("Cant drop");
-                }
             }
-            /*else if (Input.GetMouseButtonDown(1))
-            {
-                if (!carrying)
-                {
-                    //Cast a ray from the camera to his forward
-                    Ray ray = new Ray(cam.transform.position, cam.transform.forward);
-                    RaycastHit hit;
-                    if (Physics.Raycast(ray, out hit, maxDistance))
-                    {
-                        Rotable rotateScript = hit.transform.gameObject.GetComponent<Rotable>();
-                        if (rotateScript != null && rotateScript.isActiveAndEnabled)
-                        {
-                            rotateScript.clicked();
-                        }
-                    }
-                }
-            }*/
-     //   }
-    //}
+        }
+    }
 }
-   
